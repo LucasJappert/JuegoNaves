8fa02e5f224f15633adc9026af5cfca68dd34b47
Lucas Jappert <lucasnicolasjappert@gmail.com>
2022-04-29 16:44:01 -0300

    test fps

--- js/principal.js
+++ js/principal.js
@@ -19,24 +19,24 @@ var escenaUIMobile = new EscenaUIMobile();
 function Actualizar(timestamp) {
 
     //ACTUALIZO MUNDO
-    // if (juegoEnPausa == false && juegoFinalizado == false){
-    //     escenaUIMobile.Actualizar();
-    //     miEscenaFondo.Actualizar();
-    //     miNave.Actualizar();
-    //     miManagerEnemigos.Actualizar();
-    //     miManagerExplosiones.Actualizar();
-    //     miManagerProyectiles.Actualizar();
-    // } 
+    if (juegoEnPausa == false && juegoFinalizado == false){
+        escenaUIMobile.Actualizar();
+        miEscenaFondo.Actualizar();
+        miNave.Actualizar();
+        miManagerEnemigos.Actualizar();
+        miManagerExplosiones.Actualizar();
+        miManagerProyectiles.Actualizar();
+    } 
     
     //Limpiar canvas
     miEscenaFondo.Dibujar();
 
     //DIBUJO MUNDO
-    //miNave.Dibujar(); 
-    //miManagerEnemigos.Dibujar();
-    //miManagerExplosiones.Dibujar();
+    miNave.Dibujar(); 
+    miManagerEnemigos.Dibujar();
+    miManagerExplosiones.Dibujar();
     miEscenaInterfaceUsuario.Dibujar(timestamp);
-    //miManagerProyectiles.Dibujar();
+    miManagerProyectiles.Dibujar();
     escenaUIMobile.Dibujar();
 
     if (juegoFinalizado) miEscenaGameOver.Dibujar();
